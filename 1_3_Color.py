dd1ef6f7d075200830f3311c34d35fed13902162
AlfredoReyesAcosta <33937293+AlfredoReyesAcosta@users.noreply.github.com>
2020-10-20 16:57:11 -0500

    Add files via upload

---
+++ 1_3_Color.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+# loads the image in color
+img = cv2.imread('Sudoku.jpg')
+img[1:10,1:10]=[255,0,0]
+# [B,G,R]
+# Show image
+cv2.imshow('image1', img)
+
+
+# Change it to grey
+gray=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+# Show image
+cv2.imshow('image2', gray)
+
+# Save it in grey as Out
+cv2.imwrite("Out.jpg", gray)
+
+# Wait to show it
+cv2.waitKey(0)
+cv2.destroyAllWindows()
